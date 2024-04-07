684469a3b77f959b06f4c07d06a81dc8a343ce6f
-poltergeist <163550794+-poltergeist@users.noreply.github.com>
2024-04-07 18:50:27 +0300

    Adding file upload

--- myforum/forum/urls.py
+++ myforum/forum/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     path('signup/', views.user_signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
+    path('upload/', views.upload, name="upload" ),
     path('posts/', views.posts, name='posts'),
     path('add_post/', views.add_post, name='add_post'),
     path('add_comment/<int:post_id>/',
          views.add_comment, name='add_comment'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myforum/forum/views.py
+++ myforum/forum/views.py
@@ -4,6 +4,8 @@ from .forms import PostForm, CommentForm
 from .models import Post, Comment
 from .forms import SignupForm, LoginForm
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 
 
 def index(request):
@@ -87,3 +89,13 @@ def add_comment(request, post_id):
         form = CommentForm()  # Create a new form instance
     # Pass the form to the template context along with the post_id
     return render(request, 'add_comment.html', {'form': form, 'post_id': post_id})
+
+#For file upload
+def upload(request):
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(name)
+    return render(request, 'upload.html', context)
